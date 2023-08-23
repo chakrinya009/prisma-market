6baf24cad664d8f5414c7b90cff8e544f3cd31fd
chakrinya <chakrinyabalabadra@gmail.com>
2023-08-23 22:32:12 +0530

    change

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.scss";
-
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
-import Category from "./components/Category/Category";
-import SingleProduct from "./components/SingleProduct/SingleProduct";
-import Newsletter from "./components/Footer/Newsletter/Newsletter";
-import AppContext from "./utils/context";
+import Headers from './components/Headers';
+import Home from './components/Home';
+import CartDetails from './components/CartDetails';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes,Route} from "react-router-dom"
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppContext>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/:id" element={<Category />} />
-          <Route path="/product/:id" element={<SingleProduct />} />
-        </Routes>
-        <Newsletter />
-        <Footer />
-      </AppContext>
-    </BrowserRouter>
+    <>
+     <Headers />
+     <Routes>
+      <Route  path='/' element={<Home />}/>
+      <Route  path='/cart' element={<CartDetails />}/>
+     </Routes>
+     <Toaster />
+    </>
   );
 }
 
 export default App;
- 
